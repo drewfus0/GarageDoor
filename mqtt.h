0ae54c56b355ab6978799ac2cea2d517da0d23d7
drewfus <drewfus.d@gmail.com>
2020-01-11 01:49:53 +1100

    update mqtt to respond to trigger topic and clear

--- mqtt.h
+++ mqtt.h
@@ -4,38 +4,72 @@
 #define MQTT_USERNAME   "drew"
 #define MQTT_KEY        "drew33"
 IPAddress mqttIP(10, 1, 1, 10);
-#define Garage "garage/door/state"
-#define Controller "garage/door/controller"
+#define MQTT_NAME       "GarageTest"
+#define Garage "garage/door2/state"
+#define Controller "garage/door2/controller"
+#define Trigger "garage/door2/Trigger"
+
+void (*DoorToggle)() = 0;
+
+void MqttSetFuctionDoor(void (*fp)()) {
+  DoorToggle = fp;
+}
+
+void DoorT(){
+  if(0!=DoorToggle){
+    (*DoorToggle)();
+  } else
+  {
+    Serial.print("Error DoorToggle Not found");
+  }
+  
+}
+void mqttMsg(char* topic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+PubSubClient client(mqttIP, 1883, mqttMsg, wifiClient);
+long lastReconnectAttempt = 0;
 
 void mqttMsg(char* topic, byte* payload, unsigned int length){
-  String msg = (char*)payload;
+  String msg;
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i=0;i<length;i++) {
     Serial.print((char)payload[i]);
+    msg += (char)payload[i];
   }
   Serial.println();
+  if ( strcmp(topic,Trigger)==0 )
+  {
+      Serial.print("Trigger :");
+      Serial.print(msg);
+      Serial.println();
+      if (msg.equals("now"))
+      {
+          Serial.println("OpenDoor;");
+          client.publish(Trigger,"Done");
+          DoorT();
+      }
+  }
 }
-WiFiClient wifiClient;
-PubSubClient client(mqttIP, 1883, mqttMsg, wifiClient);
-long lastReconnectAttempt = 0;
 
 boolean reconnect() {
-  if (client.connect("GarageEsp8266", MQTT_USERNAME, MQTT_KEY, Controller, 0, true, "Disconnected")) {
+  if (client.connect(MQTT_NAME, MQTT_USERNAME, MQTT_KEY, Controller, 0, true, "Disconnected")) {
     // Once connected, publish an announcement...
-    client.publish(Controller,"Connected");
+    client.publish(Controller,"Connected", true);
     // ... and resubscribe
     client.subscribe(Garage);
+    client.subscribe(Trigger);
   }
   return client.connected();
 }
 
 void mqttSetup()
 {
-
+  
 }
-
+long temper = 0;
 void mqttLoop()
 {
   if (!client.connected()) {
@@ -45,7 +79,8 @@ void mqttLoop()
       // Attempt to reconnect
       Serial.print("Attempting MQTT connection...");
       if (reconnect()) {
-        lastReconnectAttempt = 0;
+        //lastReconnectAttempt = 0;
+        //Serial.print(now);
         Serial.println("Connected");
       }else
       {
